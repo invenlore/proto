48687e21655145b9c2eae1201e285fdd5e55c996
ivankprod <houseprotector@mail.ru>
2025-12-19 03:50:03 +0500

    feat(docs): add Swagger UI support

---
+++ embed.go
@@ -0,0 +1,30 @@
+package third_party
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+)
+
+//go:embed pkg/api.swagger.json
+var SwaggerJSON []byte
+
+//go:embed third_party/swagger_ui/*
+var SwaggerUI embed.FS
+
+func GetSwaggerJSON() []byte {
+	return SwaggerJSON
+}
+
+func GetSwaggerUIEmbedFS() embed.FS {
+	return SwaggerUI
+}
+
+func GetSwaggerUISubFS() (fs.FS, error) {
+	subFS, err := fs.Sub(SwaggerUI, "swagger_ui")
+	if err != nil {
+		return nil, fmt.Errorf("could not get sub filesystem for swagger_ui: %w", err)
+	}
+
+	return subFS, nil
+}
